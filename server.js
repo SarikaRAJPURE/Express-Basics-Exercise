9368637978349c99130e42c0e0e3973d30542c47
SarikaRAJPURE <sprajpure@gmail.com>
2023-01-10 14:26:44 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
-
+//array of Magic 8 ball responses…..
+let responses=["It is certain",
+                "It is decidedly so",
+                "Without a doubt",
+                "Yes definitely",
+                "You may rely on it",
+                "As I see it yes",
+                "Most likely",
+                "Outlook good",
+                "Yes",
+                "Signs point to yes",
+                "Reply hazy try again",
+                "Ask again later",
+                "Better not tell you now",
+                "Cannot predict now",
+                "Concentrate and ask again",
+                "Don't count on it",
+                "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
 //1. Greetings
 //Make a route '/greeting' that sends a generic greeting to the screen like "Hello, stranger".
 app.get('/greeting',(req,res)=>{
@@ -25,5 +42,19 @@ app.get('/tip/:total/:tipPercentage',(req,res)=>{
     tip=(+req.params.total)*(+req.params.tipPercentage)/100;
     res.send(`Tip:${tip}$`);
 });
+
+//3. Magic 8 Ball
+//Create a route of '/magic' that should expect a phrase in the URL that ask the Magic 8 ball a question.
+//Use this link to check output http://localhost:5000/magic/?phrase=Will%20I%20Be%20A%20Millionaire
+//We can't use spaces in the url, so we use %20 to express a space in the url.
+//So if the user hits that route and asks a question of "Will I be a Millionaire" 
+//he should get his question asked and a random Magic 8 ball quote on the screen.
+//**** Send the magic 8 ball response back between html <h1> tags
+app.get('/magic', (req,res) => {
+    console.log(req.query);
+    let randomResponse = Math.floor(Math.random()*responses.length);
+    
+    res.send(`<h2>Question: ${req.query.phrase}?</h2><h1>Answer: ${responses[randomResponse]}</h1>`)
+})
 
 app.listen(5000,()=>console.log("listening on port 5000"));
