52b50b0f74c7c48f67a325896cf5cae5f5cefad7
SarikaRAJPURE <sprajpure@gmail.com>
2023-01-10 13:48:34 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-//Greetings
+//1. Greetings
 //Make a route '/greeting' that sends a generic greeting to the screen like "Hello, stranger".
 app.get('/greeting',(req,res)=>{
     res.send("Hello, stranger");
@@ -13,4 +13,17 @@ app.get('/greeting',(req,res)=>{
 app.get('/greeting/:name',(req,res)=>{
     res.send(`${req.params.name} ! It's so great to see you!`);
 });
+
+//2. Tip Calculator
+//Your app should have a route of '/tip' and it should expect 2 params. 
+//One should be total and one should be tipPercentage.
+//When hitting the route, the page should display 
+//how much your tip will be based on the total amount of the bill and the tip percentage. 
+//(ex. hitting '/tip/100/20' should display 20 on the page).
+app.get('/tip/:total/:tipPercentage',(req,res)=>{
+    let tip;
+    tip=(+req.params.total)*(+req.params.tipPercentage)/100;
+    res.send(`Tip:${tip}$`);
+});
+
 app.listen(5000,()=>console.log("listening on port 5000"));
